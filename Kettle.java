6efc2a544a1b83388ad6d05f658fc61c5655b199
Yaforov <90694570+Yaforov@users.noreply.github.com>
2022-03-14 14:53:08 +0300

    Добавлены новые файлы

---
+++ Kettle.java
@@ -0,0 +1,8 @@
+class Kettle {
+    public static void main(String[] args) {
+        int water = 5;
+        System.out.println("Take a kettle");
+       System.out.println(water == 0? "Pour water" : "No need to pour");
+        System.out.println("Put the kettle on fire");
+    }
+}
